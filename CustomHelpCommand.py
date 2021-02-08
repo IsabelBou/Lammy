79b016e64faa7eaa28160d040c85d6df7e73f6c5
guy hirschorn <guydht1@gmail.com>
2021-02-09 00:16:17 +0200

    added very basic implementation of specific help command

--- CustomHelpCommand.py
+++ CustomHelpCommand.py
@@ -1,5 +1,6 @@
 from discord import Embed
 from discord.ext.commands import DefaultHelpCommand
+from discord.ext.commands.core import Command
 
 from config.config import BOT_PREFIX
 
@@ -15,3 +16,8 @@ class CustomHelpCommand(DefaultHelpCommand):
         embed.add_field(name="Nightmare Assignments", value="`assignment`, `nightmares`, `info`", inline=False)
         embed.add_field(name="Colosseum actions", value="*`start`*, *`delay`*, *`push`*, *`replace`*, *`stop`*", inline=False)
         await destination.send(embed=embed)
+
+    async def send_command_help(self, command: Command):
+        destination = self.get_destination()
+        print(command.usage, command.help, command.brief)
+        await destination.send("You asked for {} help but I'm stupid rn so no command for you".format(command.name))
