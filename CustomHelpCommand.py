ab49e9096f98535e74f0f802edac66b85108bb89
Isabel Bou <67393544+IsabelBou@users.noreply.github.com>
2021-02-12 12:21:04 +0100

    Update CustomHelpCommand.py
    Updated commands

--- CustomHelpCommand.py
+++ CustomHelpCommand.py
@@ -13,8 +13,8 @@ class CustomHelpCommand(DefaultHelpCommand):
                       description="Use `{}help [command]` for more info on a command.\nThe ones in italics are usable by *`admins only`*".format(BOT_PREFIX), color=0x75ebdb)
         embed.add_field(name="Initial Setup", value="*`setadmin`*, *`setmembers`*, *`time`*", inline=False)
         embed.add_field(name="Colosseum demon settings",
-                        value="`demonlist`, *`setdemons`*, `demons`, `demonsrearguard`, `demonsvanguard`", inline=False)
-        embed.add_field(name="Nightmare Assignments", value="`assignment`, `nightmares`, `info`", inline=False)
+                        value="`demonlist`, *`setdemons`*, `getdemons`, `demonsrearguard`, `demonsvanguard`", inline=False)
+        embed.add_field(name="Nightmare Assignments", value="`assignment`, `nightmares`, `info`, `check`, *`ask`*", inline=False)
         embed.add_field(name="Colosseum actions", value="*`start`*, *`delay`*, *`push`*, *`replace`*, *`stop`*", inline=False)
         await destination.send(embed=embed)
 
