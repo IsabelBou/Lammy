fa60453d4e373885f024b636936a689846855e9f
guy hirschorn <guydht1@gmail.com>
2021-02-12 15:07:30 +0200

    Added keep alive

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from ConfigurationSaver import ConfigurationSaver
+from keep_alive import run_keep_alive
 from Lammy import Lammy
 
 if __name__ == "__main__":
     bot = Lammy()
     conf_saver = ConfigurationSaver("configuration.p", bot)
     conf_saver.start()
+    run_keep_alive()
     bot.run()
     conf_saver.stop()
