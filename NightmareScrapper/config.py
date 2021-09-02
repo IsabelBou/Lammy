0adf602a71619a8bfb4d569d1d51ad9105d8c049
Isabel Bou <67393544+IsabelBou@users.noreply.github.com>
2021-09-02 13:23:55 +0200

    Update config.py

--- NightmareScrapper/config.py
+++ NightmareScrapper/config.py
@@ -1,6 +1,6 @@
-NM_JSON_URL = "https://raw.githubusercontent.com/sinoalice-datamine/data/master/EN/card_mst_list_en.json"
-NM_JP_JSON_URL = "https://raw.githubusercontent.com/sinoalice-datamine/data/master/JP/card_mst_list.json"
-NM_SKILLS_JSON_URL = "https://raw.githubusercontent.com/sinoalice-datamine/data/master/EN/art_mst_list.json"
+JSON_URL = "https://raw.githubusercontent.com/sinoalice-datamine/data/master/EN/card_mst_list_en.json"
+JP_JSON_URL = "https://raw.githubusercontent.com/sinoalice-datamine/data/master/JP/card_mst_list.json"
+SKILLS_JSON_URL = "https://raw.githubusercontent.com/sinoalice-datamine/data/master/EN/art_mst_list.json"
 
 ATTRIBUTE_TO_COLOR_MAPPING = {
     3: 0x89faa6,
