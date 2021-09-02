287727ac54a7b826bd4b764d08098bf1a287b071
Isabel Bou <67393544+IsabelBou@users.noreply.github.com>
2021-09-02 13:44:01 +0200

    Added weapon skill list to URLs

--- NightmareScrapper/config.py
+++ NightmareScrapper/config.py
@@ -1,6 +1,7 @@
 JSON_URL = "https://raw.githubusercontent.com/sinoalice-datamine/data/master/EN/card_mst_list_en.json"
 JP_JSON_URL = "https://raw.githubusercontent.com/sinoalice-datamine/data/master/JP/card_mst_list.json"
 SKILLS_JSON_URL = "https://raw.githubusercontent.com/sinoalice-datamine/data/master/EN/art_mst_list.json"
+WP_SKILLS_JSON_URL = "https://raw.githubusercontent.com/sinoalice-datamine/data/master/EN/skill_mst_list.json"
 
 ATTRIBUTE_TO_COLOR_MAPPING = {
     3: 0x89faa6,
